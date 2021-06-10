22365e185f5d4bbabff22b7313eb2a90a548eb3e
Alison Noyes <anoyes@caltech.edu>
2021-06-09 22:57:16 -0700

    Heroku config files

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request
+import requests
 
+xooa = "https://api.xooa.com/api/v2/"
 app = Flask(__name__)
 
 @app.route("/ping", methods=["POST"])
 def listen_ping():
     name = request.form.get("name")
     return f"Hello {name}!\n"
+
+@app.route("/authenticate", methods=["POST"])
+def listen_authenticate():
+    tok = request.form.get("token")
+    r = requests.get(xooa + "ENDPOINT", headers={"Bearer": tok})
+    print(r.text)
+    print(r.json())
+    return "found" in r.lower()
 
 if __name__ == "__main__":
     app.run()
