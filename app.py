9468ebde639f747819ce00605ab2d9c8262efc5e
Alison Noyes <anoyes@caltech.edu>
2021-06-10 17:15:53 -0700

    Get tokens of identity used for authentication

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import keyring
 import requests
 
 xooa = "https://api.xooa.com/api/v2/"
@@ -13,8 +14,20 @@ def listen_ping():
 def listen_authenticate():
     tok = request.form.get("token")
     r = requests.get(xooa + "identities/me", headers={"Authorization": "Bearer " + tok})
+    if "unauthorized" in r.text:
+        return "Authentication failed\n"
+
     name = r.json()["IdentityName"]
+    keyring.set_password("backend", "apitoken", tok)
     return f"Hello {name}!\n"
+
+@app.route("/mytokens", methods=["POST"])
+def listen_mytokens():
+    tok = keyring.get_password("backend", "apitoken")
+    if not tok:
+        return "Request failed. Please authenticate first."
+    r = requests.get(xooa + "erc721/me/tokens", headers={"Authorization": "Bearer " + tok})
+    return r.json()
 
 if __name__ == "__main__":
     app.run()
